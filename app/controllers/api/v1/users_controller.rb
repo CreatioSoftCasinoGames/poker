76530c83d91ad5b50a89c2c3e7d13c6146fa6412
Preeti Gupta <guptapreeti.india@gmail.com>
2014-09-30 20:37:07 +0530

    Added user creation api

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::UsersController < Api::V1::ApplicationController
+
+	def create
+		params[:password] = "temp1234" if params[:password].blank?
+		@user = User.new(email: params[:email], password: params[:password], password_confirmation: params[:password], first_name: params[:first_name], last_name: params[:last_name], fb_id: params[:fb_id])
+		if @user.save
+			render json: {
+				user: @user,
+				valid: true
+			}
+		else
+			render json: {
+				user: @user,
+				valid: false,
+				errors: @user.errors.full_messages.join(", ")
+			}
+		end
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :table_configs
+      resources :users
+      resources :sessions, :only => [:create]
     end
   end
 
