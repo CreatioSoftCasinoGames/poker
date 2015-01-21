3a9be238b1325c82c61b5c8f0f559bac9438e7b0
Prashant Mishra <prashant.mishra@w3villa.com>
2015-01-21 19:28:32 +0530

    Putting data from sql to redis

--- app/models/economy_hand.rb
+++ app/models/economy_hand.rb
@@ -1,2 +1,13 @@
 class EconomyHand < ActiveRecord::Base
+
+	after_save :set_data_into_redis
+
+	private
+
+	def set_data_into_redis
+		REDIS_CLIENT.SADD("economy_hands", "economy_hand:#{self.id}")
+		REDIS_CLIENT.HMSET("economy_hand:#{self.id}", "hand_quality", self.hand_quality, "winner_point", self.winner_point, "other_winner_point", self.other_winner_point)
+		
+	end
+
 end
