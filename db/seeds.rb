ae261be636bba921d84ddfb4cf16e55178fa7861
Prashant Mishra <prashant.mishra@w3villa.com>
2015-01-28 19:27:12 +0530

    Implement authorization of users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user = User.create(:email => "rishabh@creatiosoft.com", :password => "@creatiosoft", :password_confirmation => "@creatiosoft")
-user.save
+user = User.new(:email => "rishabh@creatiosoft.com", :password => "@creatiosoft", :password_confirmation => "@creatiosoft", :role => "ADMIN")
+p user.save
