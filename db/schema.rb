e59ab86b235d8e90878d44e246a55d7c7e5aa6a7
Ishank Gupta <ishank_18@yahoo.com>
2015-01-29 19:30:44 +0530

    Fixed schema conflict

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20150129091546) do
-=======
-ActiveRecord::Schema.define(version: 20150128165308) do
->>>>>>> a89ac585865c1f32041c4c2c6814b360c5423782
 
   create_table "api_keys", force: true do |t|
     t.string   "token"
