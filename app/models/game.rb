1bc91eb41285bdae12e4283c63df8ca90d1004cc
Amrendra Singh <amrendra.singh@w3villa.com>
2015-01-31 16:12:24 +0530

    xp and level fixes for sidekiq

--- app/models/game.rb
+++ app/models/game.rb
@@ -31,7 +31,8 @@ class Game < ActiveRecord::Base
 			users_attributes.push({
 				id: user.id,
 				chips: user.chips - game_user.round_chips + node_obj['remainingChips'],
-				xp: xp + node_obj['xp'],
+				xp: node_obj['xp'],
+				level: node_obj['level'],
 				biggest_pot: [user.biggest_pot, node_obj['winnerPrize']].max,
 				hands_played: (user.hands_played + 1),
 				hands_won: (node_obj['isWinner'] ? (user.hands_won + 1) : user.hands_won),
