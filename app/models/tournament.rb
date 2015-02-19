25e7d0aa59d19cfa8b4ca0b8aef5cca58cbf2908
Ishank Gupta <ishank_18@yahoo.com>
2015-02-19 18:45:50 +0530

    Added callback to generate_tournaments

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,5 +1,15 @@
 class Tournament < ActiveRecord::Base
 
 	belongs_to :tournament_config
+	scope :active, -> {where(active: true)}
+	before_update :update_config
+
+	private
+
+	def update_config
+		if self.changes.include?(:active) && !active
+			self.tournament_config.is_running = false
+		end
+	end
 
 end
--- app/models/tournament_config.rb
+++ app/models/tournament_config.rb
@@ -1,5 +1,18 @@
 class TournamentConfig < ActiveRecord::Base
 
 	has_many :tournaments
+	scope :have_no_games, -> {where(is_running: false)}
+	accepts_nested_attributes_for :tournaments
+
+	def self.generate_tournaments
+		Tournament.active.where(end_date: Date.today).each do |tournament|
+			tournament.update_attributes({active: false})
+		end
+		self.have_no_games.where(start_date: Date.today).each do |tournament_config|
+			end_date = (tournament_config.period ? (Date.today + tournament_config.days.days) : tournament_config.end_date)
+			tournament_config.attributes = {is_running: true, tournaments_attributes: [{start_date: Date.today, end_date: end_date, active: true}]}
+			tournament_config.save
+		end
+	end
 
 end
