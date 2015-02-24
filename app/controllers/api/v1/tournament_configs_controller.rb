f917fee2d06bb33506ab5e1618eced6b9b0e2fda
Prashant Mishra <prashant.mishra@w3villa.com>
2015-02-24 16:49:31 +0530

    Added my_reward and my_level in leader board

--- app/controllers/api/v1/tournament_configs_controller.rb
+++ app/controllers/api/v1/tournament_configs_controller.rb
@@ -10,10 +10,18 @@ class Api::V1::TournamentConfigsController < Api::V1::ApplicationController
 			@tournament_users = @active_tournament_users.where(user_id: friend_ids).order("chips desc").limit(100)
 			my_rank = @active_tournament_users.where(user_id: friend_ids).order('chips DESC').map(&:user_id).index(@user.id).to_f + 1
 			my_chips = @active_tournament_users.where(user_id: @user.id).first.chips
+			user_rewards = REDIS_CLIENT.ZRANGEBYSCORE("reward_level_sorted_set", my_chips, "+inf", "limit", 0, 1)
+			reward_details = REDIS_CLIENT.HGETALL(user_rewards)
+			my_reward = reward_details.in_groups_of(2).to_h["reward"]
+			my_level = reward_details.in_groups_of(2).to_h["level"]
 		else
 			@tournament_users = @tournament_config.tournaments.active.first.tournament_users.order("chips desc").limit(300)
 			my_rank = @tournament_config.tournaments.active.first.tournament_users.order('chips DESC').map(&:user_id).index(@user.id).to_f + 1
 			my_chips = @tournament_users.where(user_id: @user.id).first.chips
+			user_rewards = REDIS_CLIENT.ZRANGEBYSCORE("reward_level_sorted_set", my_chips, "+inf", "limit", 0, 1)
+			reward_details = REDIS_CLIENT.HGETALL(user_rewards)
+			my_reward = reward_details.in_groups_of(2).to_h["reward"]
+			my_level = reward_details.in_groups_of(2).to_h["level"]
 		end
 		leader_board = @tournament_users.as_json({
 			only: [:chips],
@@ -26,6 +34,8 @@ class Api::V1::TournamentConfigsController < Api::V1::ApplicationController
 			leader_board: leader_board,
 			my_rank: my_rank,
 			my_chips: my_chips,
+			my_reward: my_reward,
+			my_level: my_level,
 			remaining_time: @tournament_config.tournaments.active.first.created_at - Time.now + 24.hours
 		}
 	end
